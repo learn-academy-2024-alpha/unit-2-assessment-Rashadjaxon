eb78a8804ed9bc431f77738e3c207e040e8d51a0
Rashadjaxon <amirjacksonscholar@gmail.com>
2024-03-01 12:07:32 -0800

    Here we go

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // UNIT 2 ASSESSMENT: Coding Practical Questions with Jest
 
+//const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, leave comments to help us understand your thought process
@@ -12,41 +14,91 @@
 
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
 
+//const numbersArray1 = [6, 7, 8, 9, 10]
+//const numbersArray2 = [24, 27, 30, 33, 36] 
+
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
+// const numbersArray1 = [6, 7, 8, 9, 10]
+
+// // Expected output: [18, 21, 24, 27, 30]
+
+// const numbersArray2 = [24, 27, 30, 33, 36]
+
 // Expected output: [72, 81, 90, 99, 108]
 
 // b) Create the function that makes the test pass.
 
 // Explain your code:
 
-// --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
+// --------------------1) Create a function that takes an object as an argument and decides if the number inside it is evenly divisible by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const object1 = { number: 15 }
-// Expected output: "15 is divisible by three"
-const object2 = { number: 0 }
-// Expected output: "0 is divisible by three"
-const object3 = { number: -7 }
-// Expected output: "-7 is not divisible by three"
+    
 
-// b) Create the function that makes the test pass.
+// describe("divByThree", () => {
+//    it("takes an object as an argument and decides if the number inside it is evenly divisible by three", () => {
+//        const object1 = { number: 15 }        
+//        const object2 = { number: 0 }
+//        const object3 = { number: -7 }
 
-// Explain your code:
+//         expect(divByThree(object1)).toEqual("15 is divisible by 3")
+//         expect(divByThree(object2)).toEqual("0 is divisible by 3")
+//         expect(divByThree(object3)).toEqual("-7 is not divisible by 3")
+//     })
+// })
 
-// --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
-// a) Create a test with expect statements for each of the variables provided.
+// // // b) Create the function that makes the test pass.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+// const divByThree = (divByObj) => {
+// //let div3 =  divByObj.number % 3 === 0) {
+//     if (divByObj.number % 3 === 0) {
+//         return `${divByObj.number} is divisible by 3`
+//     } else {
+//         return `${divByObj.number} is not divisible by 3`}
+        
+// } 
+// // Explain your code:
+//  create variable accessing object with dot notation (probably not neccessary, so commented out❌)
+//  using modulo to divide by three to see if remainder equals zero       
+//  return either string based on the remainder with includes string interpulation 
+
+
+
+
+//----------------------------------------------------------------------------------------
+
+// // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
+
+// // a) Create a test with expect statements for each of the variables provided.
+
+descirbe("capitalizer", () => {
+    it("takes in an array of words and returns an array with all the words capitalized",() => {
+        const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"];
+        const randomNouns2 = ["temperature", "database", "chopsticks", "mango"];
+        expect(capitalizer(randomNouns1)).toEqual("Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew");
+        expect(capitalizer(randomNouns2)).toEqual("Temperature", "Database", "Chopsticks", "Mango");
+    })
+})
+
+
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
 
-// b) Create the function that makes the test pass.
+// // b) Create the function that makes the test pass.
 
-// Explain your code:
+//const capitalizer = (capArray) => {
+    //capNouns
+
+//}
+const capitalizer = (capArray) => {
+    // Use map to iterate through each word in the array
+    return capArray.map(word => {
+        // Capitalize the first letter of each word and make the rest lowercase
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    })
+}
+
+// // Explain your code:
--- coding-concepts.js
+++ coding-concepts.js
@@ -8,44 +8,44 @@
 
 // --------------------1) What will this log?
 
-const cohort = "Alpha 2024"
+// const cohort = "Alpha 2024"
 // console.log(cohort.split(" "))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "A", "l", "p", "h", "a", "2", "0", "2", "4"
+// b) Verify and explain:split turns the strings characters into subset of strings. The actual output: [ 'Alpha', '2024' ]
 
 // --------------------2) What will this log?
 
-const greeter = (name) => {
-  `Hello, ${name}!`
-}
+// const greeter = (name) => {
+//   `Hello, ${name}!`
+// }
 // console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer: undefined
+// // b) Verify and explain: output: undefined. This function has no return
 
-// --------------------3) What will this log?
+// // --------------------3) What will this log?
 
-const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
-// console.log(onlyOdds)
+// const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
+//   console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer: predicted output: [11,13,15]
+// // b) Verify and explain: use .filter to remove all number that can be divided by 2 using modulo and bang operator. actual output: [11, 13, 15]
 
-// --------------------4) What will this log?
+// // --------------------4) What will this log?
 
-const myCodingSkills = {
-  languages: ["JavaScript", "Ruby"],
-  frameworks: ["React", "Ruby on Rails"],
-  databases: "PostgreSQL",
-  versionControl: "GitHub"
-}
-// console.log(myCodingSkills.languages[0])
+// const myCodingSkills = {
+//   languages: ["JavaScript", "Ruby"],
+//   frameworks: ["React", "Ruby on Rails"],
+//   databases: "PostgreSQL",
+//   versionControl: "GitHub"
+// }
+// // console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer:
+// // b) Verify and explain:
 
-// --------------------5) What will this log?
+// // --------------------5) What will this log?
 
 class Learn {
   constructor(name) {
@@ -55,7 +55,7 @@ class Learn {
   }
 }
 const learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer: output: 'George', 'Alpha', 2024 
+// // b) Verify and explain: new replaces name in constructor and returns the values in name, cohort, year. actual output: Learn { student: 'George', cohort: 'Alpha', year: 2024 }
